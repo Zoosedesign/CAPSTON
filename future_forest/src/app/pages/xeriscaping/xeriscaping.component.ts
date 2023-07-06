f0244f32722a2d50ac39de16bbc20282f3219a21
Zoosedesign <andrea.zucchetti@me.com>
2023-07-06 11:38:44 +0200

    def

--- future_forest/src/app/pages/xeriscaping/xeriscaping.component.ts
+++ future_forest/src/app/pages/xeriscaping/xeriscaping.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PlantsService } from 'src/app/services/plants.service';
 
 @Component({
   selector: 'app-xeriscaping',
@@ -7,9 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class XeriscapingComponent implements OnInit {
 
-  constructor() { }
+  constructor(private PlantsSrv: PlantsService) { }
 
   ngOnInit(): void {
+    this.PlantsSrv.imgZoom('garden');
   }
-
 }
