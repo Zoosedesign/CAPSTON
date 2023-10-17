2d701d80a7d685ffd3cbc3f9f3d4f132440539e3
Zoosedesign <andrea.zucchetti@me.com>
2023-10-17 18:04:49 +0200

    def

--- future_forest_upgrade/src/app/app-routing.module.ts
+++ future_forest_upgrade/src/app/app-routing.module.ts
@@ -39,12 +39,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(
-      routes, {
-        enableTracing: false,
-        anchorScrolling: 'enabled',
-      }
-    )],
+    RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
